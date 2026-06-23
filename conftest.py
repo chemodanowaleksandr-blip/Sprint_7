59e9de55224dfe4a5f2b8008ef1bb980c4bfc9c3
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-23 21:40:38 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,22 +1,23 @@
 import pytest
-from src.courier_api import ScooterApi
+from src.courier_api import scooterapi
 from src.data import CourierData
+
 
 @pytest.fixture
 def create_and_delete_courier():
     """
     Создает курьера перед тестом.
-    После завершения теста автоматически удаляет его по ID (если логин успешен).
+    После завершения теста автоматически удаляет его по ID (если логин успешный).
     """
     payload = CourierData.generate_new_courier_payload()
-    response = ScooterApi.create_courier(payload)
-    
+    response = scooterapi.create_courier(payload)
+
     courier_id = None
-    login_response = ScooterApi.login_courier({"login": payload["login"], "password": payload["password"]})
+    login_response = scooterapi.login_courier({"login": payload["login"], "password": payload["password"]})
     if login_response.status_code == 200:
         courier_id = login_response.json().get("id")
 
     yield payload, response, courier_id
 
     if courier_id:
-        ScooterApi.delete_courier(courier_id)
+        scooterapi.delete_courier(courier_id)
