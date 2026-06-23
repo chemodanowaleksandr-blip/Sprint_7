bf733df4d8519f4b775a7e48ab4bfa87ed34582c
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-23 22:34:45 +0300

    Update courier_api.py

--- src/courier_api.py
+++ src/courier_api.py
@@ -1,33 +1,31 @@
 import requests
 import allure
+from src.data import Urls
 
 
 class scooterapi:
-    CREATE_COURIER = "https://praktikum-services.ru"
-    LOGIN_COURIER = "https://praktikum-services.ru/login"
-    ORDERS = "https://praktikum-services.ru"
 
     @staticmethod
     @allure.step("Создание курьера (POST)")
     def create_courier(payload):
-        return requests.post(scooterapi.CREATE_COURIER, json=payload)
+        return requests.post(Urls.CREATE_COURIER, json=payload)
 
     @staticmethod
     @allure.step("Логин курьера в системе (POST)")
     def login_courier(payload):
-        return requests.post(scooterapi.LOGIN_COURIER, json=payload)
+        return requests.post(Urls.LOGIN_COURIER, json=payload)
 
     @staticmethod
     @allure.step("Удаление курьера по ID (DELETE)")
     def delete_courier(courier_id):
-        return requests.delete(f"{scooterapi.CREATE_COURIER}/{courier_id}")
+        return requests.delete(f"{Urls.CREATE_COURIER}/{courier_id}")
 
     @staticmethod
     @allure.step("Создание заказа (POST)")
     def create_order(payload):
-        return requests.post(scooterapi.ORDERS, json=payload)
+        return requests.post(Urls.ORDERS, json=payload)
 
     @staticmethod
     @allure.step("Получение списка заказов (GET)")
     def get_orders_list():
-        return requests.get(scooterapi.ORDERS)
+        return requests.get(Urls.ORDERS)
