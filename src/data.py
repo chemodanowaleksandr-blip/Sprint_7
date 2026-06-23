0e756c924431731cae1850ba648dba6d179f5f2b
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-23 22:27:05 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -2,7 +2,7 @@ import random
 import string
 
 class Urls:
-    BASE_URL = "https://qa-scooter.praktikum-services.ru"
+    BASE_URL = "https://praktikum-services.ru"
     CREATE_COURIER = f"{BASE_URL}/api/v1/courier"
     LOGIN_COURIER = f"{BASE_URL}/api/v1/courier/login"
     ORDERS = f"{BASE_URL}/api/v1/orders"
