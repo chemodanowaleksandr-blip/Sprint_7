7ba67e9eb94da7939ff3a4fbbe757c2307b156c6
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-23 21:54:28 +0300

    Update courier_api.py

--- src/courier_api.py
+++ src/courier_api.py
@@ -5,27 +5,27 @@ from src.data import *
 
 class scooterapi:
 
-    @allure.step("Создание курьера (POST)")
     @staticmethod
+    @allure.step("Создание курьера (POST)")
     def create_courier(payload):
         return requests.post(CREATE_COURIER, json=payload)
 
-    @allure.step("Логин курьера в системе (POST)")
     @staticmethod
+    @allure.step("Логин курьера в системе (POST)")
     def login_courier(payload):
         return requests.post(LOGIN_COURIER, json=payload)
 
-    @allure.step("Удаление курьера по ID (DELETE)")
     @staticmethod
+    @allure.step("Удаление курьера по ID (DELETE)")
     def delete_courier(courier_id):
         return requests.delete(f"{CREATE_COURIER}/{courier_id}")
 
-    @allure.step("Создание заказа (POST)")
     @staticmethod
+    @allure.step("Создание заказа (POST)")
     def create_order(payload):
         return requests.post(ORDERS, json=payload)
 
-    @allure.step("Получение списка заказов (GET)")
     @staticmethod
+    @allure.step("Получение списка заказов (GET)")
     def get_orders_list():
         return requests.get(ORDERS)
