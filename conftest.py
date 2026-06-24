ec15e1cf5251eb7ff87421e2d2c40955950500a3
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-24 21:55:38 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
 import pytest
-from src.courier_api import scooterapi
+from src.courier_api import ScooterApi
 from src.data import CourierData
 
 @pytest.fixture
 def create_and_delete_courier():
     payload = CourierData.generate_new_couple_payload()
-    response = scooterapi.create_courier(payload)
+    response = ScooterApi.create_courier(payload)
     
     courier_id = None
-    login_response = scooterapi.login_courier({"login": payload["login"], "password": payload["password"]})
+    login_response = ScooterApi.login_courier({"login": payload["login"], "password": payload["password"]})
     if login_response.status_code == 200:
         courier_id = login_response.json().get("id")
         
     yield payload, response, courier_id
 
     if courier_id:
-        scooterapi.delete_courier(courier_id)
+        ScooterApi.delete_courier(courier_id)
