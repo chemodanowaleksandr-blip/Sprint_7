16743cbbc949340a4290b6b0a27def80fb32bfc4
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-24 21:56:44 +0300

    Update test_create_order.py

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,6 +1,6 @@
 import pytest
 import allure
-from src.courier_api import scooterapi
+from src.courier_api import ScooterApi
 from src.data import OrderData
 
 @allure.epic("Управление заказами")
@@ -18,14 +18,14 @@ class TestOrders:
         payload = OrderData.BASE_ORDER_PAYLOAD.copy()
         payload["color"] = colors
         
-        response = scooterapi.create_order(payload)
+        response = ScooterApi.create_order(payload)
         
         assert response.status_code == 201
         assert "track" in response.json()
 
     @allure.title("Получение списка заказов")
     def test_get_orders_list(self):
-        response = scooterapi.get_orders_list()
+        response = ScooterApi.get_orders_list()
         
         assert response.status_code == 200
         assert "orders" in response.json()
